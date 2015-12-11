d1648a0fe2c5e30ce796be0af68405108ac7f8e6
andrassv <andrassv@outlook.com>
2015-12-11 14:58:01 -0500

    improved the session store for production use

--- app.js
+++ app.js
@@ -7,30 +7,34 @@ var express = require('express'),
 	config = require('./config/default'),
 	hbs = require('hbs');
 
-//var models = require('./models');
-
 var app = express(),
 	server = require('http').createServer(app);
 
 app.set('port', config['server'].port);
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon('./public/images/favicon.png'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(session({
-	secret: 'someCoolSecret',
-	cookie: { maxAge: 24 * 60 * 60 * 1000 * 365 },
-	resave: false,
-	saveUninitialized: true
-}));
-
-app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs')
 app.set('view options', {
 	layout: '/layouts/default.hbs'
 });
+
+var models = require('./models');
+var SequelizeStore = require('connect-session-sequelize')(session.Store);
+app.use(session({
+	secret: 'someCoolSecret',
+	store: new SequelizeStore({
+		db: models.sequelize,
+		checkExpirationInterval: 15 * 60 * 1000,
+		expiration: 24 * 60 * 60 * 1000 * 365
+	}),
+	cookie: { maxAge: 24 * 60 * 60 * 1000 * 365 },
+	resave: false,
+	saveUninitialized: true
+}));
 
 var index = require('./routes/index.js');
 app.use('/', index);
